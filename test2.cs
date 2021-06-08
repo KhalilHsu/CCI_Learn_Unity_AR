a7488ff0a2b1b849533aef74b54c0fd8858e7404
Khalil <65265303+KhalilHsu@users.noreply.github.com>
2021-06-08 20:04:11 +0800

    Add files via upload

---
+++ test2.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.ARFoundation;
+
+
+public class test2 : MonoBehaviour
+{
+    private ARTrackedImageManager arTrackedImageManager;
+
+    private void Awake()
+    {
+        arTrackedImageManager = FindObjectOfType<ARTrackedImageManager>();
+    }
+
+    public void OnEnable()
+    {
+        arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    public void OnDisable()
+    {
+        arTrackedImageManager.trackedImagesChanged -= OnImageChanged;
+    }
+
+    public void OnImageChanged(ARTrackedImagesChangedEventArgs args)
+    {
+        foreach (var trackedImage in args.added)
+        {
+            Debug.Log(trackedImage.name);
+        }
+    }
+}
